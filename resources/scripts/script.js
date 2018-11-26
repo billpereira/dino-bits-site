3bd686a6be8e0f9928ffb688acfaea8833938041
William Pereira <william.pereira@me.com>
2018-11-26 13:47:13 -0200

    becoming responsive

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	/* Sticky Navigation */
 	$('.js--sobre').waypoint(function(direction) {
 		if (direction == 'down') {
 			$('nav').addClass('sticky');
@@ -7,5 +9,24 @@ $(document).ready(function() {
 		}
 	}, {
         offset: '75%'
+	});
+
+
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
       });
+    });
+	
+
 });
